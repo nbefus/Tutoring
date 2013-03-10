fb3bb47df836c180d269799a691143d58c7c6e5a
nbefus <nbefus@gmail.com>
2013-03-09 17:18:59 -1000

    Playing with charts

---
+++ src/TestChart.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nathaniel
+ */
+public class TestChart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          ChartTest demo = new ChartTest("Comparison", "Which operating system are you using?");
+          demo.pack();
+          demo.setVisible(true);
+          
+          /*
+         BarChartTest demo2 = new BarChartTest("Bar Chart Demo");
+        demo2.pack();
+        //RefineryUtilities.centerFrameOnScreen(demo2);
+        demo2.setVisible(true);*/
+    }
+}
