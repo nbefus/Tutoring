0dec7687ff41e2f95ce1346b6a0dea018199ae5f
Precious <pbinas@gmail.com>
2013-03-17 14:00:10 -1000

    mkm

--- src/tutoring/entity/SaveFile.java
+++ src/tutoring/entity/SaveFile.java
@@ -1,44 +1,63 @@
 package tutoring.entity;
+import  tutoring.helper.*;
 
-
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
-public class SaveFile 
+public class SaveFile extends JFrame
 {
-
-
-    public static void main(String[] args) 
+    private static List data;
+    private static boolean isSaved = false;
+    private static String savingFeedback = "There's is no data to be saved. File not created.";
+    
+    public SaveFile(List data)
     {
-        //make sure the the file does not exist
-        //gather data
-        //choose location, press save, saves file
-        //PrintWriter
-        
-        JFileChooser window = new JFileChooser();
-        String filename = null;
-        window.showSaveDialog(window);
-        
-        try 
-        {
-            PrintWriter outputFile = new PrintWriter(filename);
-        } 
-        catch (FileNotFoundException ex) 
-        {
-            Logger.getLogger(SaveFile.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        //savesFile() returns boolean
-        //setPath
-        //getPath
-        //setFilename
-        //getFilename
-        //getFeedback
+        this.data = data;
+    }
+    
+    public SaveFile()
+    { 
+    }
+    
+    public boolean isSaved()
+    {
+        return isSaved;
+    }
+    
+    public String savingFeedback()
+    {
+        return savingFeedback;
+    }
+    
+    public void saveFile() throws FileNotFoundException
+    {
+        File file = null;
+        FileWriter fWriter; //ask for name
+        PrintWriter outputFile;
+        JFileChooser dialog = new JFileChooser();
+        dialog.showSaveDialog(this);
+        file = dialog.getSelectedFile();
+        file = file.getAbsoluteFile();
+        outputFile = new PrintWriter(file);
+        outputFile.println(data);
+        isSaved = true;
         
     }
+    
+    public static void main(String args[]) throws FileNotFoundException
+    {
+        SaveFile save = new SaveFile((ArrayList<User>) HibernateTest.select("from Subjects"));
+        save.saveFile();
+    }
+
 }
